3a9cb05190681e109d57961fcd9ca7ae171ca1c9
Oleg <olegbyta@gmail.com>
2021-09-01 20:31:24 +0300

    update

--- js/task-01.js
+++ js/task-01.js
@@ -4,6 +4,7 @@ console.log(`В списке ${allElCategories.length} категории`);
 
 allElCategories.forEach(item => {
    const nameCategories = item.querySelector('h2');
-   const numberElements = item.querySelector('li');
-   console.log(`Категория: ${nameCategories.textContent}, Количество элементов: ${numberElements.length}`);
+   const numberElements = item.querySelectorAll('li');
+   console.log(`Категория: ${nameCategories.textContent}`)
+   console.log(`Количество элементов: ${numberElements.length}`);
 })
