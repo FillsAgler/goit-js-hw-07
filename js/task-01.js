048658d410e530f72162afd2caf41e8ddd139dc7
Oleg <olegbyta@gmail.com>
2021-09-01 20:18:16 +0300

    Add task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,9 @@
 const listCategories = document.querySelector('#categories');
 const allElCategories = listCategories.querySelectorAll('.item');
 console.log(`В списке ${allElCategories.length} категории`);
+
+allElCategories.forEach(item => {
+   const nameCategories = item.querySelector('h2');
+   const numberElements = item.querySelector('li');
+   console.log(`Категория: ${nameCategories.textContent}, Количество элементов: ${numberElements.length}`);
+})
