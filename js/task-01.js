a811c1d6b69be5697f5c75165e06f5090db00b6c
Oleg <olegbyta@gmail.com>
2021-09-03 12:39:56 +0300

    Add task 2

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,6 @@
 const listCategories = document.querySelector('#categories');
 const allElCategories = listCategories.querySelectorAll('.item');
 console.log(`В списке ${allElCategories.length} категории`);
-console.log();
 
 allElCategories.forEach(item => {
    const nameCategories = item.querySelector('h2');
---
+++ js/task-02.js
@@ -0,0 +1,17 @@
+const ingredients = [
+   'Картошка',
+   'Грибы',
+   'Чеснок',
+   'Помидоры',
+   'Зелень',
+   'Приправы',
+];
+
+const listEl = document.querySelector('#ingredients');
+
+const allEl = ingredients.map(item => {
+   const liEl = document.createElement('li');
+   liEl.textContent = item;
+});
+
+listEl.append (...allEl)
